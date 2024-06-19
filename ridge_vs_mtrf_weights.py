769a4fd238c7152d03057f6a857b19070b010395
joctavio287 <joctavio287@gmail.com>
2024-06-18 22:25:54 -0300

    added porcentual relative difference

--- ridge_vs_mtrf_weights.py
+++ ridge_vs_mtrf_weights.py
@@ -1,23 +1,40 @@
 import numpy as np, os, mne, matplotlib.pyplot as plt
-from Funciones import load_pickle
+from funciones import load_pickle
 
+# ==================================================================
+# Read and extract weights to compute porcentual relative difference
 path_mtrf_weights = 'saves/mtrf/Escucha/Original/stims_Normalize_EEG_Standarize/tmin-0.2_tmax0.6/stim_Envelope_EEG_band_Theta/total_weights_per_subject_Envelope_Theta.pkl'
 path_ridge_weights = 'saves/Ridge/Escucha/Original/Stims_Normalize_EEG_Standarize/tmin-0.6_tmax0.2/Stim_Envelope_EEG_Band_Theta/Pesos_Totales_Envelope_Theta.pkl'
 
+# Compute porcentual relative difference
 w_mtrf = load_pickle(path=path_mtrf_weights)['average_weights_subjects'].mean(axis=0)[:,0,:]
 w_ridge = np.flip(load_pickle(path=path_ridge_weights), axis=1)
-w_resta = w_ridge-w_mtrf
+w_rel_diff = 100*(w_ridge-w_mtrf)/(np.abs(w_mtrf+w_ridge)/2)
 
-sr=128
-tmin, tmax = -.2, .6
+# TODO: there are values that goes to inf and -inf. Son 5
+number_of_infs = np.unique(w_rel_diff==-np.inf, return_counts=True)[1][1] + np.unique(w_rel_diff==np.inf, return_counts=True)[1][1]
+w_rel_diff[w_rel_diff==np.inf]=0
+w_rel_diff[w_rel_diff==-np.inf]=0
+ 
+# w_rel_diff = np.abs(w_ridge+w_mtrf)/2
+# w_difference = w_ridge-w_mtrf
+
+# ========================================
+# Define hyperparameters used in the model
+
+# Times and delays
+sr, tmin, tmax = 128, -.2, .6
 delays = np.arange(int(np.round(tmin * sr)), int(np.round(tmax * sr) + 1))
 times = (delays/sr)
 
+# Topographic information
 montage = mne.channels.make_standard_montage('biosemi128')
 info = mne.create_info(ch_names=montage.ch_names, sfreq=sr, ch_types='eeg').set_montage(montage)
 
+# ========================================
 # Make plot of weights difference
-fig, ax = plt.subplots(nrows=1, ncols=3, layout='tight', figsize=(12,4), sharey=True)
+
+fig, ax = plt.subplots(nrows=3, ncols=1, layout='tight', figsize=(6,8), sharex=True)
 
 # Ridge
 evoked_ridge = mne.EvokedArray(data=w_ridge, info=info)
@@ -47,6 +64,7 @@ ax[0].plot(
     linewidth=2)
 
 # Graph properties
+ax[0].set(xlabel='')
 ax[0].legend()
 ax[0].grid(visible=True)
 ax[0].set_title('RIDGE')
@@ -79,20 +97,22 @@ ax[1].plot(
     linewidth=2)
 
 # Graph properties
+ax[1].set(xlabel='')
 ax[1].legend()
-ax[1].set_ylabel('')
+# ax[1].set_ylabel('')
 ax[1].grid(visible=True)
 ax[1].set_title('MTRF')
 
-# Difference
-evoked_resta = mne.EvokedArray(data=w_resta, info=info)
+# Porcentual relative difference
+evoked_diff = mne.EvokedArray(data=w_rel_diff, info=info)
 
 # Relabel time 0
-evoked_resta.shift_time(times[0], relative=True)
+evoked_diff.shift_time(times[0], relative=True)
 
 # Plot
-evoked_resta.plot(
+evoked_diff.plot(
     scalings={'eeg':1}, 
+    exclude='bads',
     zorder='std', 
     time_unit='ms',
     show=False, 
@@ -105,7 +125,7 @@ evoked_resta.plot(
 # Add mean of all channels
 ax[2].plot(
     times * 1000, #ms
-    evoked_resta._data.mean(0), 
+    evoked_diff._data.mean(0), 
     'k--', 
     label='Mean', 
     zorder=130, 
@@ -113,8 +133,12 @@ ax[2].plot(
 
 # Graph properties
 ax[2].legend()
-ax[2].set_ylabel('')
+ax[2].set_ylabel(r'$\epsilon_{\%}$')
+ax[2].set(xlabel='Time(ms)')
 ax[2].grid(visible=True)
-ax[2].set_title('RESTA')
+ax[2].set_title('Relative Porcentual difference')
+ymin = evoked_diff._data.mean(axis=0).min()
+ymax = evoked_diff._data.mean(axis=0).max()
+ax[2].set_ylim((ymin, ymax))
 
 plt.show()
