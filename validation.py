45498412c759d778357de32c1f227ef7878f9b45
joctavio287 <joctavio287@gmail.com>
2025-01-01 10:32:01 -0300

    changing names

--- validation.py
+++ validation.py
@@ -8,7 +8,7 @@ from joblib import Parallel, delayed
 
 # Modules
 from funciones import load_pickle, dump_pickle, dict_to_csv, iteration_percentage, Suppress_print
-from model_parallelization import parallel_fold_model
+from model_implementations import parallel_fold_model
 from plot import hyperparameter_selection
 from load import load_data
 import config
