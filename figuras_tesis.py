d9551d874296a023ae7cd245bc66f8fe37f3ba8d
joctavio287 <joctavio287@gmail.com>
2025-03-22 18:17:38 -0300

    prediccion eeg figura tesis

--- figuras_tesis.py
+++ figuras_tesis.py
@@ -6736,18 +6736,18 @@ fig.show()
 
 # ==========================
 # EEG prediction vs original: 21, 2
-weights = load_pickle(r'saves\mtrf_ridge_torch\External\weights\stims_Normalize_EEG_Standarize\tmin-0.2_tmax0.6\Theta\Envelope\total_weights_per_subject.pkl')['average_weights_subjects']
+from load import load_data
+from processing import shifted_matrix
+
+situation='External'
+stim = 'Phonological'
+band ='Theta'
+weights = load_pickle(f'saves/mtrf_ridge_torch/External/weights/stims_Normalize_EEG_Standarize/tmin-0.2_tmax0.6/Theta/{stim}/total_weights_per_subject.pkl')['average_weights_subjects']
 weights = weights.mean(axis=0).mean(axis=1)
 
 eeg = load_pickle(r'saves\preprocessed_data\External\tmin-0.2_tmax0.6\EEG\Theta\Causal\Sesion21.pkl')[1]
-
-situation='External'
-stim = 'Envelope'
-band ='Theta'
 preprocessed_data_path = r'saves\preprocessed_data\External\tmin-0.2_tmax0.6'
 
-from load import load_data
-from processing import shifted_matrix
 sujeto_1, sujeto_2, samples_info = load_data(
                                             sesion=21,
                                             stim=stim,
@@ -6759,15 +6759,16 @@ sujeto_1, sujeto_2, samples_info = load_data(
                                             situation=situation
                                             )
 
-design_matrix = shifted_matrix(features=sujeto_2['Envelope'], delays=config.delays)
+design_matrix = shifted_matrix(features=sujeto_2[stim], delays=config.delays)
 
+eeg_predict = design_matrix @ weights.T
 eeg -= eeg.mean(axis=0)
 eeg /= eeg.std(axis=0)
 eeg_predict -= eeg_predict.mean(axis=0)
 eeg_predict /= eeg_predict.std(axis=0)
 
 plt.figure(figsize=(8,4))
-plt.plot(eeg.mean(1)[:2000], label='Original', color='blue')
-plt.plot(eeg_predict.mean(1)[:2000], label='Predicción', color='green')
+plt.plot(eeg.mean(1)[:], label='Original', color='blue')
+plt.plot(eeg_predict.mean(1)[:], label='Predicción', color='green')
 plt.legend()
 plt.show()
